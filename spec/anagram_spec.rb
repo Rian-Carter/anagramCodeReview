c980ec4a5fbf99094506d974b312ecfea0aaa6de
Rian Carter <R.Carter86@hotmail.com>
2022-04-22 09:53:41 -0700

    add first test

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -1,2 +1,9 @@
 require('rspec')
 require('anagram')
+
+describe ('Anagram_Checker') do
+  it('checks to see if two words are anagrams') do
+    red = Anagram_Checker.new('ailed', 'ideal')
+    expect(red.anagram()).to(eq("Success! These words are anagrams!"))
+  end
+end
