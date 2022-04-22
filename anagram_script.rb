0f718d80867a421652fffd38e54698afa0621e32
Rian Carter <R.Carter86@hotmail.com>
2022-04-22 14:54:48 -0700

    change output to user specifying the words they entered

--- anagram_script.rb
+++ anagram_script.rb
@@ -5,4 +5,4 @@ puts "Please enter two words to check if they are or are not anagrams."
 word1 = gets.chomp
 word2 = gets.chomp
 test1 = Anagram_Checker.new(word1, word2)
-puts "#{word1} and #{word2}: #{test1.anagram}"
+puts "You entered #{word1} and #{word2}: #{test1.anagram}"
