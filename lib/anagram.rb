a8d6ff4dea5af115d7855f417c59839aedf177ff
Rian Carter <R.Carter86@hotmail.com>
2022-04-22 09:56:28 -0700

    add code to pass first test

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 require('pry')
+
+class Anagram_Checker
+  def initialize(word1, word2)
+    @word1 = word1
+    @word2 = word2
+  end
+
+  def anagram
+    array1 = @word1.split("")
+    array2 = @word2.split("")
+binding.pry
+    array1.include?(array2)
+    return "Success! These words are anagrams!"
+  end
+end
