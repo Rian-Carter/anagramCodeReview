54d3b5e2dc8a47dad4bc91ca23b988f18e2a46bf
Rian Carter <R.Carter86@hotmail.com>
2022-04-22 14:52:34 -0700

    add script to take in input from user

--- anagram_script.rb
+++ anagram_script.rb
@@ -1,2 +1,8 @@
 #!/usr/bin/env ruby
 require('./lib/anagram.rb')
+
+puts "Please enter two words to check if they are or are not anagrams."
+word1 = gets.chomp
+word2 = gets.chomp
+test1 = Anagram_Checker.new(word1, word2)
+puts "#{word1} and #{word2}: #{test1.anagram}"
