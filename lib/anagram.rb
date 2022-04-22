6685faf325c04af7410ddf0655344d9c8d138bf9
Rian Carter <R.Carter86@hotmail.com>
2022-04-22 14:52:09 -0700

    fix spacing and indentation

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,13 +10,12 @@ class Anagram_Checker
     array1 = @word1.downcase.split("")
     array2 = @word2.downcase.split("")
     vowels = /[aeiouy]/
-    
+
     if array1.any? { |array1| array2.include?(array1) } === false
       return "There are no matching letters. Looks like you have an antigram!"
     elsif array1.all? { |array1| array2.include?(array1) } === true && array1.length === array2.length
-    # binding.pry
       return "Success! These words are anagrams!"
-    else array1.include?(vowels) === false || array2.include?(vowels) === false
+    elsif array1.include?(vowels) === false || array2.include?(vowels) === false
       return "Please enter a real word."
     end
   end
