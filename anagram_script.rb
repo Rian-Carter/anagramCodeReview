e468f0c628d2a1946cc3ea33abbf2c394a477b1e
Rian Carter <R.Carter86@hotmail.com>
2022-04-23 11:23:22 -0700

    remove 'and' from puts statement as to not always print to screen

--- anagram_script.rb
+++ anagram_script.rb
@@ -8,4 +8,4 @@ puts "Please enter two words to check if they are or are not anagrams."
 word1 = gets.chomp
 word2 = gets.chomp
 test1 = Anagram_Checker.new(word1, word2)
-puts "You entered #{word1} and #{word2}: #{test1.anagram} and #{test1.palindrome}"
+puts "You entered #{word1} and #{word2}: #{test1.anagram} #{test1.palindrome}"
