347604948a01a8a6154ca448b6fcefc8d7d8c0b9
Rian Carter <R.Carter86@hotmail.com>
2022-04-23 11:17:22 -0700

    add put statements and include palindrome checker

--- anagram_script.rb
+++ anagram_script.rb
@@ -1,8 +1,11 @@
 #!/usr/bin/env ruby
 require('./lib/anagram.rb')
 
+puts "---Hello---"
+puts "-Welcome to Anagram Checker-"
+puts "-I will also determine if words are palindromes-"
 puts "Please enter two words to check if they are or are not anagrams."
 word1 = gets.chomp
 word2 = gets.chomp
 test1 = Anagram_Checker.new(word1, word2)
-puts "You entered #{word1} and #{word2}: #{test1.anagram}"
+puts "You entered #{word1} and #{word2}: #{test1.anagram} and #{test1.palindrome}"
